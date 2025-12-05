3ef2f58cb06161ab191d3bc3a4d920ee1a36d6b7
Dejan Jarc <jarcdejan5@gmail.com>
2025-12-05 09:43:08 +0100

    Init repo

---
+++ src/main/kotlin/traversium/ModerationService/ModerationServiceApplication.kt
@@ -0,0 +1,11 @@
+package traversium.ModerationService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModerationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ModerationServiceApplication>(*args)
+}
